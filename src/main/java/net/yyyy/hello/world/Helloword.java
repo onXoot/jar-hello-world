0e6719a8001a021f501a01ddea42a366e5b3b89a
beetlerx <beetlerx@gmail.com>
2018-09-04 23:37:45 +0800

    jar upload demo

---
+++ src/main/java/net/yyyy/hello/world/Helloword.java
@@ -0,0 +1,12 @@
+package net.yyyy.hello.world;
+
+public class Helloword {
+
+    public String hello() {
+        return "hello world";
+    }
+
+    public String hello2() {
+        return "hello word#2";
+    }
+}
